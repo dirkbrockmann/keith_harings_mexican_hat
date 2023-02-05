f89c349d99f0767966e3a4f5e260e438bbc9ca02
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-02-05 16:31:42 +0100

    changed display resolution to reduced anti aliasing

--- src/container_config.js
+++ src/container_config.js
@@ -5,7 +5,7 @@ export default {
 	display_border:true,
 	debug_lattice:"debug-grid-16",
 	controls_grid:{nx:12,ny:12},
-	display_size: {width:500,height:500},
+	display_size: {width:505,height:505},
 	controls_size: {width:480,height:480},
 	display_class:"fl w-100 w-50-ns pa2 ph3-ns",
 	controls_class:"fl w-100 w-50-ns pa2 ph3-ns",
